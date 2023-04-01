c153db84a5760e7211c9ceb8adf65aefbf4b4482
RyukXGitHub <129211074+RyukXGitHub@users.noreply.github.com>
2023-04-01 21:33:34 +0530

    Update __main__.py

--- KratosXBot/__main__.py
+++ KratosXBot/__main__.py
@@ -209,9 +209,7 @@ def start(update: Update, context: CallbackContext):
         else:
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
-                 escape_markdown(uptime),
-                    sql.num_users(),
-                    sql.num_chats()),  
+                  
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
@@ -441,9 +439,7 @@ def Kratos_about_callback(update: Update, context: CallbackContext):
         first_name = update.effective_user.first_name
         query.message.edit_text(
             PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
-             escape_markdown(uptime),
-                    sql.num_users(),
-                    sql.num_chats()), 
+             
             reply_markup=InlineKeyboardMarkup(buttons),
             parse_mode=ParseMode.MARKDOWN,
             timeout=60,
@@ -483,9 +479,7 @@ And Using [SqlAlchemy](https://www.sqlalchemy.org) & [MongoDB](https://cloud.mon
         first_name = update.effective_user.first_name
         query.message.edit_text(
             PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
-            escape_markdown(uptime),
-                    sql.num_users(),
-                    sql.num_chats()),  
+           
             reply_markup=InlineKeyboardMarkup(buttons),
             parse_mode=ParseMode.MARKDOWN,
             timeout=60,
