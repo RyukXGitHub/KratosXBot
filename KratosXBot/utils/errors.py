a6f05b7eedeb26959ef660a12a7c91c22a21877c
RyukXGitHub <129211074+RyukXGitHub@users.noreply.github.com>
2023-04-02 18:08:01 +0530

    Update errors.py

--- KratosXBot/utils/errors.py
+++ KratosXBot/utils/errors.py
@@ -2,10 +2,9 @@ import sys
 import traceback
 from functools import wraps
 
-from KratosXBot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001771852812)
+from FallenRobot import OWNER_ID, pbot
 
 
 def split_limits(text):
@@ -13,7 +12,7 @@ def split_limits(text):
         return [text]
 
     lines = text.splitlines(True)
-    small_msg = ''
+    small_msg = ""
     result = []
     for line in lines:
         if len(small_msg) + len(line) < 2048:
@@ -21,7 +20,7 @@ def split_limits(text):
         else:
             result.append(small_msg)
             small_msg = line
- else:
+
     result.append(small_msg)
 
     return result
@@ -33,26 +32,24 @@ def capture_err(func):
         try:
             return await func(client, message, *args, **kwargs)
         except ChatWriteForbidden:
-            await app.leave_chat(message.chat.id)
             return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             errors = traceback.format_exception(
-                etype=exc_type, value=exc_obj, tb=exc_tb,
+                exc_type,
+                value=exc_obj,
+                tb=exc_tb,
             )
             error_feedback = split_limits(
-                '**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n"'.format(
+                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
                     0 if not message.from_user else message.from_user.id,
                     0 if not message.chat else message.chat.id,
                     message.text or message.caption,
-                    ''.join(errors),
+                    "".join(errors),
                 ),
             )
             for x in error_feedback:
-                await app.send_message(
-                    LOG_GROUP_ID,
-                    x
-                )
+                await pbot.send_message(OWNER_ID, x)
             raise err
 
     return capture
